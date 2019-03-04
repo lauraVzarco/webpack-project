a50019ed74d524f98b81b3d553422d3aa34646ad
lauraVzarco <laurav.zarco@gmail.com>
2019-03-04 11:01:12 +0100

    functional project

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import './index.css';
-import App from './containers/App.jsx';
+import './App.css';
+import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
 
